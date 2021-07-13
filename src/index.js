071e270445f93de5f50e4b984efec2eaca2dff71
Alexandra Khutorna <khutornaalexandra@gmail.com>
2021-07-13 17:31:06 +0300

    useEffect practical

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, {useState, Component, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState(1);
     const [visible, setVisible] = useState(true);
 
 
@@ -15,8 +15,10 @@ const App = () => {
                 <button
                     onClick = {() => setVisible(false)}
                 >hide</button>
-                <ClassCounter value = {value}/>
-                <HookCounter value = {value}/>
+                {/* <ClassCounter value = {value}/>
+                <HookCounter value = {value}/> */}
+                {/* <Notification/> */}
+                <PlanetInfo id = {value}/>
             </div>
         )
     } else {
@@ -28,28 +30,65 @@ const App = () => {
     }
 }
 
+const PlanetInfo = ({id}) =>{
+
+    const [name, setName] = useState(null)
+
+    useEffect(() =>{
+        let cancelled = false;
+
+        fetch(`https://swapi.dev/api/planets/${id}`)
+        .then(res => res.json())
+        .then(data => !cancelled && setName(data.name));
+
+        return () => cancelled = true;
+    }, [id])
+
+    return (
+        <div>{id} - {name}</div>
+    )
+}
+
 const HookCounter = ({value}) => {
 
     useEffect(() => {
-        console.log('useEffect');
+        console.log('mount');
+        return () => console.log('unmount')
+    }, [])
 
-        return () => console.log('clear')
-    }, [value])
+    useEffect(() =>  console.log('update'))
     return <p>{value}</p>
+}
+
+const Notification = () => {
+
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setVisible(false), 3500)
+        return () => clearTimeout(timeout)
+    }, [])
+
+
+    return (
+        <div>
+            {visible && <p>hello</p>}
+        </div>
+    )
 }
 
 class ClassCounter extends Component{
 
     componentDidMount() {
-        console.log('mount')
+        console.log('class: mount')
     }
 
     componentDidUpdate(){
-        console.log('update')
+        console.log('class: update')
     }
 
     componentWillUnmount(){
-        console.log('unmount')
+        console.log('class: unmount')
     }
 
     render(){
